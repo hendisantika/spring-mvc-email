45033bb0b57429198d33775b7c6cbb90f70af358
Hendi Santika <hendisantika@gmail.com>
2017-08-15 06:10:47 +0700

    #5 - Added Email Service

---
+++ src/main/java/com/hendisantika/springmvcemail/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springmvcemail.service;
+
+import org.springframework.mail.SimpleMailMessage;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/15/17
+ * Time: 6:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EmailService {
+    void sendSimpleMessage(String to,
+                           String subject,
+                           String text);
+    void sendSimpleMessageUsingTemplate(String to,
+                                        String subject,
+                                        SimpleMailMessage template,
+                                        String ...templateArgs);
+    void sendMessageWithAttachment(String to,
+                                   String subject,
+                                   String text,
+                                   String pathToAttachment);
+}
